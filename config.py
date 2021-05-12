55cfa2101592b0091d6b73854362eac86ab0e1bd
Greg Stillman <gj.stillman@gmail.com>
2021-05-12 09:23:56 -0500

    secrets removed into separate file
    secrets removed into separate file using the config.py and gitignore method for the twitter API keys

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 
 # .gitignore should include reference to config.py
+
+# CAO_CAO_BOT1 KEYS
+consumer_key_cao_cao = "zQoIV3A5rNcK5XwleoYrXnBLd"
+consumer_secret_cao_cao ="yaqZWVyCRxl3xXKrczt1v0hdfyKlwcBdhbddfmJXZHSyDYlUVq"
 api_key = "YOUR_KEY"
 api_secret = "YOUR_SECRET"
 access_token = "YOUR_ACCESS_TOKEN"
