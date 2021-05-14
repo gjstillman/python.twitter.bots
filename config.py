f26dedb3a5c1ea1a1e8a4eb39b9fa221ff4024c8
Greg Stillman <gj.stillman@gmail.com>
2021-05-14 15:56:45 -0500

    converting to AWS Lambda
    converting to AWS Lambda

--- config.py
+++ config.py
@@ -4,7 +4,6 @@
 # CAO_CAO_BOT1 KEYS
 consumer_key_cao_cao = "zQoIV3A5rNcK5XwleoYrXnBLd"
 consumer_secret_cao_cao ="yaqZWVyCRxl3xXKrczt1v0hdfyKlwcBdhbddfmJXZHSyDYlUVq"
-api_key = "YOUR_KEY"
-api_secret = "YOUR_SECRET"
-access_token = "YOUR_ACCESS_TOKEN"
-token_secret = "YOUR_TOKEN_SECRET"
+access_token_cao_cao = "1386718712795967489-i2i5PLp2mBXbZ4paLTstaZxFXRUw6E"
+token_secret_cao_cao = "5AblumOJGCms00PzJw2ike9q01IUtXNFnLaKlMzRqatp1"
+user_pin_input = "3864030"
