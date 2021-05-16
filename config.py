954172b1fca783d017f96e91c6fc09ef4ae638fc
Greg Stillman <71041136+gjstillman@users.noreply.github.com>
2021-05-16 15:50:33 -0500

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-
-# .gitignore should include reference to config.py
-
-# CAO_CAO_BOT1 KEYS
-consumer_key_cao_cao = "zQoIV3A5rNcK5XwleoYrXnBLd"
-consumer_secret_cao_cao ="yaqZWVyCRxl3xXKrczt1v0hdfyKlwcBdhbddfmJXZHSyDYlUVq"
-access_token_cao_cao = "1386718712795967489-i2i5PLp2mBXbZ4paLTstaZxFXRUw6E"
-token_secret_cao_cao = "5AblumOJGCms00PzJw2ike9q01IUtXNFnLaKlMzRqatp1"
-user_pin_input = "3864030"
